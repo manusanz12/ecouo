53611ab83235ef5851bfa646886016eb41e3e0d4
Manu Ortega <manuel.ortega@soyuo.mx>
2023-06-23 17:14:27 -0600

    Se creo validación de modulo de programas

--- ajax/data-programs.php
+++ ajax/data-programs.php
@@ -6,6 +6,17 @@ require_once "../controllers/template.controller.php";
 class DatatableController{
 
 	public function data(){
+		session_start();
+		/****Validar permisos */
+		require "../views/modules/pvalidate.php";
+			
+			
+		/***********************************************
+		 Validar permisos
+		***********************************************/
+		$p_update=Pvalidate::Validatepermit("Update",$_SESSION["validates"]->permit_role);
+		$p_suspend=Pvalidate::Validatepermit("Suspend",$_SESSION["validates"]->permit_role);
+		$p_delete=Pvalidate::Validatepermit("Delete",$_SESSION["validates"]->permit_role);
 
 		if(!empty($_POST)){
 
@@ -152,33 +163,54 @@ class DatatableController{
 					
 					if($value->estatus_program!=2){	
 							
-							$actions ="<a class='btn btn-success btn-sm rounded-circle stopcatItem' idcatItem='".base64_encode($value->id_program."~".$_GET["token"])."' table='programs' suffix='program' page='programs'>
+						if (isset($p_suspend)){
+							
+							$actions ="<a class='btn btn-success btn-sm rounded-circle stopcatItem' idcatItem='".base64_encode($value->id_program."~".$_GET["token"])."' table='programs' suffix='program'  page='programs'>
 
 										<i class='fas fa-eye'></i>
 
 										</a>
 										";
+						}
+						else{
+							$actions ="";
+						}
+
 					}
 					else{
-							$actions ="<a class='btn btn-secondary btn-sm rounded-circle activecatItem' idactivecatItem='".base64_encode($value->id_program."~".$_GET["token"])."' table='programs' suffix='program' page='programs'>
+						if (isset($p_suspend)){
+							$actions ="<a class='btn btn-secondary btn-sm rounded-circle activecatItem' idactivecatItem='".base64_encode($value->id_program."~".$_GET["token"])."' table='programs' suffix='program'  page='programs'>
 
 										<i class='fas fa-eye-slash'></i>
 
 										</a>
 										";
-					}					
+						}
+						else{
+							$actions ="";
+						}
+					}			
+					if (isset($p_update)){		
+							
 							$actions .= "<a href='/programs/edit/".base64_encode($value->id_program."~".$_GET["token"])."' class='btn btn-warning btn-sm mr-1 rounded-circle'>
 							
 										<i class='fas fa-pencil-alt'></i>
 
-										</a>
-
-										<a class='btn btn-danger btn-sm rounded-circle removecatItem' idcatItem='".base64_encode($value->id_program."~".$_GET["token"])."' table='programs' suffix='program'  page='programs'>
+										</a>";
+						}
+						else{
+							$actions .="";
+						}
+						if (isset($p_delete)){
+								$actions .= "<a class='btn btn-danger btn-sm rounded-circle removecatItem' idcatItem='".base64_encode($value->id_program."~".$_GET["token"])."' table='programs' suffix='program'  page='programs'>
 
 										<i class='fas fa-trash'></i>
 
 										</a>";
-
+						}
+						else{
+								$actions .="";
+						}
 							
 
 							$actions = TemplateController::htmlClean($actions);
