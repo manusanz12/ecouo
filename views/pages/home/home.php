70490668902df4dd6fdf01e1dfabe2d0f8fa9412
Manu ortega <manuel.ortega@soyuo.mx>
2023-10-31 17:31:51 -0600

    Se intengro el top banner con información de la tabla de noticias

--- views/pages/home/home.php
+++ views/pages/home/home.php
@@ -1,3 +1,28 @@
+<?php
+/*=============================================
+Traer el total de noticias y eventos
+=============================================*/
+
+$url = "noticies?select=id_noticie";
+$method = "GET";
+$fields = array();
+$header = array();
+
+$datanews = CurlController::request($url, $method, $fields, $header);
+
+
+
+if($datanews->status == 200){
+
+    $totalnews = $datanews->total;
+
+}else{
+
+    $totalnews = 0;
+}
+
+?>
+
 
 <section class="content-header">
   
--- views/pages/home/top_banner.php
+++ views/pages/home/top_banner.php
@@ -1,15 +1,29 @@
-	<div class="ps-block--promotion-header bg--cover"  style="background: url(../views/assets/img/banner/top/header-promotion.jpg);">
-        <div class="container">
+<?php 
+
+$randomId = rand(1, $totalnews);
+
+$url = "relations?rel=noticies,categories&type=noticie,category&linkTo=id_noticie&equalTo=".$randomId."&select=url_category,top_banner_noticie,url_noticie";
+$method = "GET";
+$fields = array();
+$header = array();
+
+$randomNews = CurlController::request($url, $method, $fields, $header)->results[0];
+$topBanner = json_decode($randomNews->top_banner_noticie, true);
+?>
+    
+    
+    <div class="ps-block--promotion-header bg--cover"  style="background: url(../views/assets/img/noticies/<?php echo $randomNews->url_category ?>/top/<?php echo $topBanner["IMG tag"] ?>);">
+            <div class="container">
             <div class="ps-block__left">
-                <h3>20%</h3>
+                <h3><?php echo $topBanner["H3 tag"] ?></h3>
                 <figure>
-                    <p>Discount</p>
-                    <h4>For Books Of March</h4>
-                </figure>
+                <p><?php echo $topBanner["P1 tag"]  ?></p>
+                <h4><?php echo $topBanner["H4 tag"] ?></h4>
+            </figure>
             </div>
             <div class="ps-block__center">
-                <p>Enter Promotion<span>Sale2019</span></p>
-            </div><a class="ps-btn ps-btn--sm" href="#">Shop now</a>
+            <p><?php echo $topBanner["P2 tag"] ?><span><?php echo $topBanner["Span tag"] ?></span></p>
+            </div><a class="ps-btn ps-btn--sm" href="<?php echo $path.$randomNews->url_category ?>"><?php echo $topBanner["Button tag"] ?></a>
         </div>
     </div>
 
