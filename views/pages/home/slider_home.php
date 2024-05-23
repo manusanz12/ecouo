8fa55ebac654d506de8e54c3e118c0d23644953b
Manu ortega <manuel.ortega@soyuo.mx>
2024-05-23 11:20:57 -0600

    modificaciones en slider

--- views/pages/home/slider_home.php
+++ views/pages/home/slider_home.php
@@ -1,29 +1,64 @@
+<?php
+/*=============================================
+Traer noticias aleatoriamente
+=============================================*/
+
+
+$randomStart = rand(0, ($totalnews-3));
+
+$select = "url_category,horizontal_slider_noticie,url_noticie,default_banner_noticie,name_noticie";
+
+$url = "relations?rel=noticies,categories&type=noticie,category&orderBy=id_noticie&orderMode=ASC&startAt=".$randomStart."&endAt=5&select=".$select;
+$method = "GET";
+$fields = array();
+$header = array();
+
+$NewsHSlider = CurlController::request($url, $method, $fields, $header)->results;
+
+?>
+    
     <div class="ps-home-banner">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+
+
+
+
             <ol class="carousel-indicators">
             <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleControls" data-slide-to="1"></li>
             <li data-target="#carouselExampleControls" data-slide-to="2"></li>
             </ol>
-
+            <?php
+            $V_active="active";
+            ?>
             <div class="carousel-inner">
-                <div class="carousel-item active" data-background="img/slider/horizontal/1.jpg">
+            <?php foreach ($NewsHSlider as $key => $value): ?>
+
+                <?php 
+
+                    $hSlider = json_decode($value->horizontal_slider_noticie, true);
+
+                ?>    
+                <div class="carousel-item <?php echo $V_active; ?>" data-background="/views/assets/img/noticies/<?php echo $value->url_category  ?>/horizontal/<?php echo $topBanner["IMG tag"] ?>);">
                     <div class="ps-banner--market-4" >
-                            <img src="img/slider/horizontal/1.jpg"  alt="...">
+                            <img src="/views/assets/img/noticies/<?php echo $value->url_category  ?>/horizontal/<?php echo $topBanner["IMG tag"] ?>);"  alt="...">
                             <div class="ps-banner__content">
-                                <h4>Limit Edition</h4>
-                                <h3>HAPPY SUMMER <br/> 
-                                    COMBO SUPER COOL <br/> 
-                                    <p>UP TO <strong> 40%</strong></p>
-                                </h3>
-                                <a class="ps-btn" href="#">Shop Now</a>
+                            <h4><?php echo $hSlider["H4 tag"] ?></h4>
+                            <h3><?php echo $hSlider["H3-1 tag"] ?><br/> 
+                                <?php echo $hSlider["H3-2 tag"] ?><br/> 
+                                <p> <?php echo $hSlider["H3-3 tag"] ?> <strong>  <?php echo $hSlider["H3-4s tag"] ?></strong></p>
+                            </h3>
+                            <a class="ps-btn" href="<?php echo $path.$value->url_product ?>"> <?php echo $hSlider["Button tag"] ?></a>
                             </div>
                     </div>        
                 </div>
-                <div class="carousel-item">
+            <?php 
+            $V_active="";
+            endforeach ?>
+                <div class="carousel-item ">
                 <img src="img/slider/horizontal/2.jpg" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item">
+                <div class="carousel-item ">
                 <img src="img/slider/horizontal/3.jpg" class="d-block w-100" alt="...">
                 </div>
             </div>
