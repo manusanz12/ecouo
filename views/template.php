3e97b7cfcdccf8a7d0f57a91003e9b5f1f57d317
Manu ortega <manuel.ortega@soyuo.mx>
2025-09-11 10:54:02 -0600

    se deb usar el htacces

--- views/template.php
+++ views/template.php
@@ -11,9 +11,13 @@ $path = TemplateController::path();
 Capturar las rutas de la URL
 =============================================*/
 
+
 $routesArray = explode("/", $_SERVER['REQUEST_URI']);
 $routesArray = array_filter($routesArray);
+//echo '<pre>'; print_r($_SERVER['REQUEST_URI']); echo '</pre>';
 //echo '<pre>'; print_r($routesArray); echo '</pre>';
+
+
 /*=============================================
 Limpiar la Url de variables GET
 =============================================*/
